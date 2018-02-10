fbfe0a95bb531874def3982c5448e21024b40833
Deshi Xiao <xiaods@gmail.com>
2018-02-11 00:56:34 +0800

    add discoveryclient

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,12 +2,13 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class Application {
 
 	@RequestMapping("/")
@@ -19,5 +20,6 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
+
+
